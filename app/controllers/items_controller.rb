5a27e5945fe6fb3e4a70da2ef7bef8e84fc21972
Lindsay Taylor <llindsayttaylor@gmail.com>
2022-12-15 12:05:52 -0500

    commit tree e905b16f0cecfb1e8f324b224b0e4f6d6fe7a853

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,47 @@
 class ItemsController < ApplicationController
 
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = find_user
+      items = user.items
+      render json: items, include: :user, status: :ok
+    else 
+      items = Item.all
+      render json: items, include: :user
+    end
+  end
+
+  def show
+    if params[:user_id]
+      user = find_user
+      item = user.items.find(params[:id])
+      render json: item, include: :user, status: :ok
+    end
+  end
+
+  def create
+    if params[:user_id]
+      user = find_user
+      item = user.items.create!(item_params)
+      render json: item, include: :user, status: :created
+    end
+  end
+
+
+  private
+
+  def render_not_found_response
+    render json: {message: "User not found"}, status: :not_found
+  end
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
   end
 
 end
